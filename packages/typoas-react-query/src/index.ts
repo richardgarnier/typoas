a6ba71cd31faceba4aea995909b02cb37d9359d2
Richard Garnier <garnier.richard.s@gmail.com>
2026-08-27 10:38:31 +0200

    Export types from types.ts in index.ts

--- packages/typoas-react-query/src/index.ts
+++ packages/typoas-react-query/src/index.ts
@@ -3,3 +3,4 @@ export * from './infinite-query-factory.js';
 export * from './mutation-factory.js';
 export * from './query-factory.js';
 export { getQueryFunctionKey } from './func-names.js';
+export * from './types.ts'
